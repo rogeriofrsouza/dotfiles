b537c9066cff1ad4f233de938374a25cd5422a1d
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2024-07-06 00:44:24 -0300

    feat(mason): improve list extensions and add mason-lspconfig

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -3,15 +3,27 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "lemminx", "sonarlint-language-server", "google-java-format" })
+      vim.list_extend(opts.ensure_installed, {
+        "sonarlint-language-server",
+        "google-java-format",
+      })
     end,
   },
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = {
-      ensure_installed = {
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
         "sqlfluff",
-      },
-    },
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "lemminx" })
+    end,
   },
 }
