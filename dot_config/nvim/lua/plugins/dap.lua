847b8fdedff99860890afae065ad274b81a47a28
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2024-08-07 00:59:21 -0300

    feat(nvim): rename dap file, change xml lsp config and remove vim-tmux-navigator keys' description

---
+++ dot_config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,16 @@
+return {
+  "mfussenegger/nvim-dap",
+  opts = function()
+    local dap = require("dap")
+
+    dap.configurations.java = {
+      {
+        type = "java",
+        request = "attach",
+        name = "Debug (Attach) - Remote",
+        hostName = "127.0.0.1",
+        port = 5005,
+      },
+    }
+  end,
+}
