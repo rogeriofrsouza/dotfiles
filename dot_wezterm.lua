8e8b4026af87f07d7d5f5dc4bd93c523dacfcad5
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2024-08-07 21:18:57 -0300

    feat(wezterm): updated font, paddings and removed term

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -2,24 +2,22 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 10.75
+config.font_size = 10.85
 
--- config.font = wezterm.font_with_fallback({
--- 	"JetBrains Mono",
--- })
+config.font = wezterm.font_with_fallback({
+	"JetBrains Mono",
+	"MesloLGS Nerd Font Mono",
+})
 
 config.enable_tab_bar = false
 
 config.color_scheme = "Tokyo Night Moon"
 
-config.term = "xterm-256color"
-
 config.window_padding = {
-	top = 2,
+	top = 4,
 	right = 0,
 	bottom = 0,
-	left = 2,
+	left = 0,
 }
 
 config.initial_cols = 132
